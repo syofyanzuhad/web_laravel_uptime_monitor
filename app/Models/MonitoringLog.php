db10bffc780a0299c2503b980c62aed4a4da3375
Syofyan Zuhad 🇵🇸 <sofyanzuhad2@gmail.com>
2024-09-02 22:19:40 +0700

    Update MonitoringLog.php

--- app/Models/MonitoringLog.php
+++ app/Models/MonitoringLog.php
@@ -5,15 +5,24 @@ namespace App\Models;
 use App\Models\CustomerSite;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 
 class MonitoringLog extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
 
     protected $fillable = ['customer_site_id', 'url', 'response_time', 'status_code'];
 
     public function customerSite()
     {
         return $this->belongsTo(CustomerSite::class)->withDefault(['name' => 'n/a']);
+    }
+
+    /**
+     * Get the prunable model query.
+     */
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subMonth());
     }
 }
