e5d2f900d8fd8ba1313103c624e37134e25fcc50
Syofyan Zuhad 🇵🇸 <sofyanzuhad2@gmail.com>
2024-09-02 22:22:03 +0700

    Update MonitoringLog.php

--- app/Models/MonitoringLog.php
+++ app/Models/MonitoringLog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\CustomerSite;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 
